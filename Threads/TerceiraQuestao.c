7ffabc44153e118ac8fcdc668c3304bbb865fd97
koga25 <66087274+koga25@users.noreply.github.com>
2020-11-04 18:20:27 -0300

    Update TerceiraQuestao.c

--- Threads/TerceiraQuestao.c
+++ Threads/TerceiraQuestao.c
@@ -14,6 +14,7 @@ char subString[30];
 unsigned short stringLength;
 unsigned short subStringLength;
 unsigned short currentPosition = 0;
+unsigned char offset = 0;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* Substrings(void* arg);
@@ -21,16 +22,29 @@ int GetThreadQuantity(char* string, char* subString);
 
 int main()
 {
+    unsigned short i = 0;
     //initializing counter
     pthread_t* newThread;
     
     unsigned short threadQuantity;
 
     //getting the strings
-    printf("type your string (0 < n <= 800)\n");    
+    FILE* file = fopen("file.txt", "r");
+    int c;
+    while((c = getc(file)) != EOF &&  c != ' ')
+    {
+        string[i] = c;
+    }
+    i = 0;
+    while((c = getc(file))&& c != EOF)
+    {
+        subString[i] = c;
+    }
+
+    /*printf("type your string (0 < n <= 800)\n");    
     fgets(string, sizeof(string), stdin);   
     printf("type your subString (0 < n <= 20)\n");
-    fgets(subString, sizeof(subString), stdin);
+    fgets(subString, sizeof(subString), stdin);*/
 
     threadQuantity = GetThreadQuantity(string, subString);
     printf("length of string: %d    length of substring: %d     threads: %d\n",
@@ -38,10 +52,16 @@ int main()
 
     //creating n threads
     newThread = (pthread_t*) malloc (sizeof(pthread_t) * threadQuantity);
-    for (int i = 0; i < threadQuantity; ++i)
-        pthread_create(&newThread[i], NULL, Substrings, NULL);
-    
+    offset = stringLength/threadQuantity;
 
+    
+    for (int i = 0; i < threadQuantity; ++i)
+    {
+        int* offsetOfSpecificThread = (int*)malloc(sizeof(int));
+        offsetOfSpecificThread[0] = offset * i;
+        pthread_create(&newThread[i], NULL, Substrings, (void*) (offsetOfSpecificThread));
+    }
+    
     //wait for all threads to finish.
     for (int i = 0; i < threadQuantity; ++i)
         pthread_join(newThread[i], NULL);
@@ -55,20 +75,21 @@ int main()
 
 void* Substrings(void* arg)
 {
-    int position = 0;
+
+    int* currentPosition = (int*)arg;
+    int maxPosition = currentPosition[0]+offset;
     //local variable used to check if it was a substring
     unsigned char isEqual = FALSE;
-    while(currentPosition < stringLength)
+    while(currentPosition[0] < maxPosition)
     {
-        position = currentPosition;
         //if the first position is true, then assign true to isEqual and begin substring check
-        if(string[position] == subString[0])
+        if(string[currentPosition[0]] == subString[0])
         {
             isEqual = TRUE;
             //checks to see if all positions are equal
             for (unsigned char i = 1; i < subStringLength-1; i++)
             {   //if a position isn't equal, then put isEqual to false and get out of loop
-                if(string[position + i] != subString[i])
+                if(string[currentPosition[0] + i] != subString[i])
                 {
                     isEqual = FALSE;
                     break;
@@ -84,18 +105,17 @@ void* Substrings(void* arg)
             }
             
         }
-        pthread_mutex_lock(&mutex);
-        currentPosition++;
-        pthread_mutex_unlock(&mutex);
+        currentPosition[0]++;
     }
+    free(arg);
     pthread_exit(NULL);
     return NULL;
 }
 
 int GetThreadQuantity(char* string, char* subString)
 {
-    stringLength = strlen(string);
-    subStringLength= strlen(subString);
-    unsigned short threadQuantity = (stringLength/subStringLength) - 1;
+    stringLength = strlen(string) - 1;
+    subStringLength= strlen(subString) - 1;
+    unsigned short threadQuantity = (stringLength/subStringLength);
     return threadQuantity;
 }
